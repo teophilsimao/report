72212c53b5ef61d9b0361b544d1f6d9abcfbe99b
Teophil Simao <teophilsimao@gmail.com>
2024-04-01 23:00:25 +0200

    first commit

---
+++ symfony/app/src/Controller/LuckyControllerTwig.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyControllerTwig extends AbstractController
+{
+    #[Route("/", name: "home")]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    #[Route("/report", name: "report")]
+    public function number(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+}
